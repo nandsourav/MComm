1c7393e924f8c6e62d65793c70f6b703d34d9f8c
chinmaywanpal <chinmay.wanpal@gmail.com>
2013-11-05 19:48:58 -0500

    Modified by chinmay

--- src/simulatepath/MovMolecules.java
+++ src/simulatepath/MovMolecules.java
@@ -2,7 +2,7 @@ package simulatepath;
 
 public class MovMolecules {
 	public static void main(String[] args){
-		System.out.println("Starting of our molecular communication project1");
+		System.out.println("Starting of our molecular communication project123");
 		
 	}
 }
